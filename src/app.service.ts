53eb47957f8d40370618aa520d9dfaf99804bca7
@a.malkov_automacon.ru <a.malkov@automacon.ru>
2025-02-13 12:41:57 +0700

    add response data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Res } from '@nestjs/common';
+import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
+import { Response } from 'express';
+
+dotenv.config();
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!!!123';
+  async getHello(): Promise<string> {
+
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: process.env.DATABASE_URL ? true : false
+    });
+
+    const users = await pool.query(`SELECT * FROM person`)
+
+    return JSON.stringify({ res: "test", data: users.rows });
   }
 }
