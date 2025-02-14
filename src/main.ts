f619b47bb3222806fd4db38c2893f40f92b37ed8
@a.malkov_automacon.ru <a.malkov@automacon.ru>
2025-02-15 00:42:00 +0700

    fix session env string

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 import { createClient } from 'redis';
 
 import * as session from 'express-session'
-import { ConfigService } from '@nestjs/config'
 import { ms, StringValue } from './libs/common/utils/ms.util'
 import { parseBoolean } from './libs/common/utils/parse-boolean.util'
 import { RedisStore } from 'connect-redis'
@@ -16,32 +15,31 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = app.get(ConfigService)
   const redis = await createClient({ url: process.env.REDIS_URL }).connect();
 
-  // app.use(
-  //   (session as any)({  // ругается vercel без as any
-  //     secret: config.getOrThrow<string>('SESSION_SECRET'),
-  //     name: config.getOrThrow<string>('SESSION_NAME'),
-  //     resave: true,
-  //     saveUninitialized: false,
-  //     cookie: {
-  //       domain: config.getOrThrow<string>('SESSION_DOMAIN'),
-  //       maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
-  //       httpOnly: parseBoolean(
-  //         config.getOrThrow<string>('SESSION_HTTP_ONLY')
-  //       ),
-  //       secure: parseBoolean(
-  //         config.getOrThrow<string>('SESSION_SECURE')
-  //       ),
-  //       sameSite: 'lax'
-  //     },
-  //     store: new RedisStore({
-  //       client: redis,
-  //       prefix: config.getOrThrow<string>('SESSION_FOLDER')
-  //     })
-  //   })
-  // )
+  app.use(
+    (session as any)({  // ругается vercel без as any
+      secret: process.env.SESSION_SECRET,
+      name: process.env.SESSION_NAME,
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        domain: process.env.SESSION_DOMAIN,
+        maxAge: ms(process.env.SESSION_MAX_AGE as StringValue),
+        httpOnly: parseBoolean(
+          process.env.SESSION_HTTP_ONLY
+        ),
+        secure: parseBoolean(
+          process.env.SESSION_SECURE
+        ),
+        sameSite: 'lax'
+      },
+      store: new RedisStore({
+        client: redis,
+        prefix: process.env.SESSION_FOLDER
+      })
+    })
+  )
 
   await app.listen(process.env.APPLICATION_PORT ?? 3000);
 }
