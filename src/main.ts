de9accc38900e9d29805cacd14689e4ee3f109ac
@a.malkov_automacon.ru <a.malkov@automacon.ru>
2025-02-15 00:07:39 +0700

    add redis

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { createClient } from 'redis';
+
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
+
+  const redis = await createClient({ url: process.env.REDIS_URL }).connect();
+
+  await redis.set('key', 'value');
+  const value = await redis.get('key');
+
+  console.log(value);
 
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.APPLICATION_PORT ?? 3000);
