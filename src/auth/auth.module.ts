a3da108c524623abc9cc8dcd64b64ca2fb59714c
@a.malkov_automacon.ru <a.malkov@automacon.ru>
2025-02-15 17:02:19 +0700

    remove alias

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import {  Module } from '@nestjs/common'
 
-import { UserService } from '@/user/user.service'
+import { UserService } from '../user/user.service'
 
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
