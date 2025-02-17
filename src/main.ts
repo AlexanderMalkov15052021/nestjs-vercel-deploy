30cc8ceb73410a0d45b189c817ffc1cbc18f3871
@a.malkov_automacon.ru <a.malkov@automacon.ru>
2025-02-17 17:21:23 +0700

    fix sameSite session

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
         secure: parseBoolean(
           process.env.SESSION_SECURE
         ),
-        sameSite: 'lax'
+        sameSite: 'none'
       },
       store: new RedisStore({
         client: redis,
