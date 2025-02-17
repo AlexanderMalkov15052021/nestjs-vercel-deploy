d3135c739cacd35167deb907936f614425e63284
@a.malkov_automacon.ru <a.malkov@automacon.ru>
2025-02-17 18:40:16 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
         secure: parseBoolean(
           process.env.SESSION_SECURE
         ),
-        sameSite: 'false'
+        sameSite: 'lax'
       },
       store: new RedisStore({
         client: redis,
